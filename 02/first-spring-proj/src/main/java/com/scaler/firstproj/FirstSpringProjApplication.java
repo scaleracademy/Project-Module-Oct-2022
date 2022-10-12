7b2840f6e86f19b3fc84d7b28ef816b5658d95de
Arnav Gupta <championswimmer@gmail.com>
2022-10-12 21:18:35 +0530

    create a basic Spring project via Spring Initializr
    Signed-off-by: Arnav Gupta <championswimmer@gmail.com>

---
+++ 02/first-spring-proj/src/main/java/com/scaler/firstproj/FirstSpringProjApplication.java
@@ -0,0 +1,13 @@
+package com.scaler.firstproj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstSpringProjApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FirstSpringProjApplication.class, args);
+    }
+
+}
---
+++ 02/first-spring-proj/src/test/java/com/scaler/firstproj/FirstSpringProjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scaler.firstproj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstSpringProjApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
