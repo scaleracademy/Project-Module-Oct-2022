7c3e82f24a13bd3058050fe47431cb52fef82ac1
Arnav Gupta <championswimmer@gmail.com>
2022-10-19 10:39:39 -0700

    blogging app, assignment 04
    Signed-off-by: Arnav Gupta <championswimmer@gmail.com>

---
+++ 04/blogapp/src/main/java/com/scaler/blogapp/BlogappApplication.java
@@ -0,0 +1,13 @@
+package com.scaler.blogapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlogappApplication.class, args);
+    }
+
+}
